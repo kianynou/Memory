f16b7e3ab6bbf8670b35a589740ffafdec0d5438
HakimDai <hakim69400@live.com>
2019-09-25 14:29:24 +0200

    affichage des cartes aleatoirement

--- card_random_display.js
+++ card_random_display.js
@@ -1,26 +1,18 @@
 
-        let randomNumbers =[];
-        let allFrontTab = getElementsByClassName('front');
+let randomNumbers =[];
+let allFrontTab = [];
+allFrontTab = document.getElementsByClassName('front');
 
-        for(i=0;i<12;i++){
-            let number = Math.round((Math.random()*11)+1);
-
-            while(randomNumbers.includes(number)){
-                number = Math.round((Math.random()*11)+1);
-            }
-            randomNumbers.push(number);
-        
-            
-            }
-        for(i=0;i<12;i++){
-            if (randomNumbers[i]>6){
-                randomNumbers[i]=randomNumbers[i]-6;
-                allFrontTab[i].src = 'images/carte'+ randomNumbers[i] + '.jpg';
-        
-            }
-            
-
-            
-            
-        }
-        
+for(let i = 0; i < 12; i++){
+    let number = Math.round((Math.random() * 11) + 1);
+    while(randomNumbers.includes(number)){
+        number = Math.round((Math.random() * 11) + 1);
+    }
+    randomNumbers.push(number);
+}
+for(let i = 0; i < 12; i++){
+    if (randomNumbers[i] > 6){
+        randomNumbers[i] = randomNumbers[i] - 6;       
+    }
+    allFrontTab[i].src = 'images/carte'+ randomNumbers[i] + '.jpg';
+}        
