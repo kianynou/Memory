7609a538f170430885e5b58008cbc6493bc6ba08
Ludivine Coujard <ludcoujard@gmail.com>
2019-09-26 20:11:57 +0200

    ajout de l'effet flip, marche sur la 1ere carte

--- card_random_display.js
+++ card_random_display.js
@@ -14,53 +14,100 @@ const frontImages = [
     'images/carte10.jpg',
     ];
 
-
+    function flip() {
+        document.getElementById("cardinner").style.transform = "rotateY(180deg)";
+    };
 //récupérer le nombre choisi par le joueur --------------------------
-let cardsNumber = 12;
+let cardsNumberChoice = 12;
 
 // Génération de l'affichage aléatoire ------------------------------
-let cardsPlayed = cardsNumber/2
+function randomDisplay(cardsNumber){
+    let cardsPlayed = cardsNumber/2
 
-let randomNumbers =[];
+    let randomNumbers =[];
 
-cardsElt = document.getElementById('cards');
-    //Génération d'un tableau de nombres aléatoires de 1 à cardsNumber
-for(let i = 0; i < cardsNumber ; i++){
-    let number = Math.round((Math.random() * (cardsNumber-1)) + 1);
-    while(randomNumbers.includes(number)){
-        number = Math.round((Math.random() * (cardsNumber-1)) + 1);
+    cardsElt = document.getElementById('cards');
+        //Génération d'un tableau de nombres aléatoires de 1 à cardsNumber
+    for(let i = 0; i < cardsNumber ; i++){
+        let number = Math.round((Math.random() * (cardsNumber-1)) + 1);
+        while(randomNumbers.includes(number)){
+            number = Math.round((Math.random() * (cardsNumber-1)) + 1);
+        }
+        randomNumbers.push(number);
     }
-    randomNumbers.push(number);
+        //On divise par 2 
+    for(let i = 0; i < cardsNumber; i++){
+        if (randomNumbers[i] > (cardsNumber/2)){
+            randomNumbers[i] = randomNumbers[i] - (cardsNumber/2);       
+        }
+        //affichage aléatoire des cartes
+    cardsElt.innerHTML += `<div class="card" id="card" onclick='flip()'>
+            <div class="flip-card-inner" id="cardinner">
+                <div class="back">
+                    <img src="${backImage}" class="border">
+                </div>
+                <div class="front">
+                <img src="images/carte${randomNumbers[i]}.jpg" class="border" id="card${i}">
+                </div>
+            </div>
+        </div>`;
+    /*cardsElt.innerHTML += `<img src="${backImage}" class="back">`;
+    cardsElt.innerHTML += `<img src="images/carte${randomNumbers[i]}.jpg" class="front" id="card${i}">`;*/
+    console.log(randomNumbers);        
+        }     
 }
-    //On divise par 2 
-for(let i = 0; i < cardsNumber; i++){
-    if (randomNumbers[i] > (cardsNumber/2)){
-        randomNumbers[i] = randomNumbers[i] - (cardsNumber/2);       
-    }
-    //affichage aléatoire des cartes
-    cardsElt.innerHTML += `<img src="${backImage}" class="back">`;
-    cardsElt.innerHTML += `<img src="images/carte${randomNumbers[i]}.jpg" class="front" id="card${i}">`;
-            
-}  
 
-// affichage au clique de "Back" vers "Front"
-let frontElt = document.getElementsByClassName('front');
+randomDisplay(12);
+
+// affichage au clique de "Back" vers "Front" ----------------------------
+/*let frontElt = document.getElementsByClassName('front');
+let backElt = document.getElementsByClassName('back');
+
 for(let i = 0; i<frontElt.length; i++){
     frontElt[i].style.display = "none";
  }
   
-let backElt = document.getElementsByClassName('back');
+
 cardIndex = [];
+srcImgTab = [];
+let score = 0;
 
 for (let i=0; i<frontElt.length; i++){
     cardIndex[i] = document.getElementById(`card${i}`);}
      
 for (let i=0; i<backElt.length; i++){
     backElt[i].addEventListener('click', function showTheFront(){ 
+
         // Comment intégrer l'effet Flip ?
+
+        //Affichage de front, disparition du back
         cardIndex[i].style.display = "block";
-        backElt[i].style.display = "none";
+        backElt[i].style.display = "none";*/
+/*
+//Comparaison des resultats -----------------------------------------------
+        let srcOk = document.getElementById('card'+i).src;
+           srcImgTab.push(srcOk);
+            console.log(srcImgTab);
+
+            if(srcImgTab.length===2){
+                // A partir de 2 cartes retournées, une alert apparaît
+                 if(srcImgTab[0]===srcImgTab[1]){
+                     alert('bravo !');
+                     score += 1;}
+                 else{
+                     alert('bouh!');
+                 }
+             }
+                 // Plus de 2 cartes, une alerte averti que c'est 2 maxi !
+             else if (srcImgTab.length>2){
+                 alert('2 cartes maxi SVP !');
+             }    
+        
+         
 
  });
- }
+}
+
+console.log(score)*/
+ 
  
