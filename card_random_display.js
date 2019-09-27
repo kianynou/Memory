408030a01164347f1d4c69b2cf69324a1b3259c6
Ludivine Coujard <ludcoujard@gmail.com>
2019-09-27 10:39:09 +0200

    ajout de flip et comparaison

--- card_random_display.js
+++ card_random_display.js
@@ -14,11 +14,9 @@ const frontImages = [
     'images/carte10.jpg',
     ];
 
-    function flip() {
-        document.getElementById("cardinner").style.transform = "rotateY(180deg)";
-    };
 //récupérer le nombre choisi par le joueur --------------------------
-let cardsNumberChoice = 12;
+let cardsNumberChoice = 2;
+
 
 // Génération de l'affichage aléatoire ------------------------------
 function randomDisplay(cardsNumber){
@@ -41,52 +39,35 @@ function randomDisplay(cardsNumber){
             randomNumbers[i] = randomNumbers[i] - (cardsNumber/2);       
         }
         //affichage aléatoire des cartes
-    cardsElt.innerHTML += `<div class="card" id="card" onclick='flip()'>
-            <div class="flip-card-inner" id="cardinner">
+        cardsElt.innerHTML += `<div class="card" id="card">
+            <div class="flip-card-inner" id="cardinner${i}">
                 <div class="back">
                     <img src="${backImage}" class="border">
                 </div>
                 <div class="front">
-                <img src="images/carte${randomNumbers[i]}.jpg" class="border" id="card${i}">
+                    <img src="images/carte${randomNumbers[i]}.jpg" class="border" id="card${i}">
                 </div>
             </div>
-        </div>`;
-    /*cardsElt.innerHTML += `<img src="${backImage}" class="back">`;
-    cardsElt.innerHTML += `<img src="images/carte${randomNumbers[i]}.jpg" class="front" id="card${i}">`;*/
-    console.log(randomNumbers);        
-        }     
+        </div>`;}
+        
 }
 
-randomDisplay(12);
+randomDisplay(cardsNumberChoice);
 
-// affichage au clique de "Back" vers "Front" ----------------------------
-/*let frontElt = document.getElementsByClassName('front');
-let backElt = document.getElementsByClassName('back');
 
-for(let i = 0; i<frontElt.length; i++){
-    frontElt[i].style.display = "none";
- }
-  
+// Fonction Flip -------------------------------------------------
 
-cardIndex = [];
-srcImgTab = [];
-let score = 0;
+let cardinnerElt = [];
+for (let i=0; i<cardsNumberChoice; i++){
+    cardinnerElt[i] = document.getElementById(`cardinner${i}`);
+}
 
-for (let i=0; i<frontElt.length; i++){
-    cardIndex[i] = document.getElementById(`card${i}`);}
-     
-for (let i=0; i<backElt.length; i++){
-    backElt[i].addEventListener('click', function showTheFront(){ 
-
-        // Comment intégrer l'effet Flip ?
-
-        //Affichage de front, disparition du back
-        cardIndex[i].style.display = "block";
-        backElt[i].style.display = "none";*/
-/*
-//Comparaison des resultats -----------------------------------------------
-        let srcOk = document.getElementById('card'+i).src;
-           srcImgTab.push(srcOk);
+for (let i=0; i<cardsNumberChoice; i++){
+    cardinnerElt[i].addEventListener('click', function flip(){ 
+        cardinnerElt[i].style.transform = "rotateY(180deg)";
+        
+        let srcImg = document.getElementById('card'+i).src;
+           srcImgTab.push(srcImg);
             console.log(srcImgTab);
 
             if(srcImgTab.length===2){
@@ -96,18 +77,15 @@ for (let i=0; i<backElt.length; i++){
                      score += 1;}
                  else{
                      alert('bouh!');
+                     cardinnerElt[i].style.transform = "rotateY(180deg)";
+
                  }
              }
                  // Plus de 2 cartes, une alerte averti que c'est 2 maxi !
              else if (srcImgTab.length>2){
                  alert('2 cartes maxi SVP !');
              }    
-        
-         
 
- });
-}
-
-console.log(score)*/
- 
- 
+    
+        });
+    }
