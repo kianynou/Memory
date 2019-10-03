b836820f1658b0e5444441ef4d6636ff78d1f66c
Ludivine Coujard <ludcoujard@gmail.com>
2019-10-03 17:44:22 +0200

    reparation score joueur solo

--- card_random_display.js
+++ card_random_display.js
@@ -29,6 +29,7 @@ class Player{
 }
 const player1 = new Player(true);
 const player2 = new Player(false);
+let alone = true;
 
 let scorePlayer1Elt = document.getElementById('scorePlayer1');
 let scorePlayer2Elt = document.getElementById('scorePlayer2');
@@ -128,7 +129,8 @@ function randomDisplay(cardsNumber,playersNumber){
     else{
         frameScore2Elt.style.display = "initial";
         frameCounterElt.style.display = "none";
-        frameScore1Elt.style.backgroundColor = "#78f875";}
+        frameScore1Elt.style.backgroundColor = "#78f875";
+        alone = false;}
 
     
 
@@ -192,7 +194,7 @@ for (let i=0; i<cardsNumberChoice; i++) {
             setTimeout(function(){alertWinner.style.display ="none"}, 1300);
             cardinnerArray = []; 
 
-            if(player1.isPlaying=== true){
+            if(player1.isPlaying || alone){
                 player1.score += 1;
             }
             else{
@@ -205,7 +207,7 @@ for (let i=0; i<cardsNumberChoice; i++) {
             setTimeout(flipBack, 1900);
             setTimeout(alertLooser.style.display ="block", 1600);
             setTimeout(function(){alertLooser.style.display ="none"}, 1300);
-                     
+            if(!alone){         
             if(player1.isPlaying){
                 player1.isPlaying = false;
                 player2.isPlaying = true;
@@ -216,6 +218,7 @@ for (let i=0; i<cardsNumberChoice; i++) {
                 player2.isPlaying = false;
                 frameScore1Elt.style.backgroundColor = "#78f875";
                 frameScore2Elt.style.backgroundColor = "#D6EAF8";}
+            }
             }
             srcImgTab = [];
                 
